c00b1b93d6445b45b6aabebb7c16e349454f24da
Raushan Raj <raushanraj1205@gmail.com>
2025-01-06 02:07:38 +0530

    basic app created

---
+++ app/component/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <>
+      <p>Hello from Navbar</p>
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center">
+      <p>Hello welcome to 3D shop</p>
+    </div>
+  );
+}
